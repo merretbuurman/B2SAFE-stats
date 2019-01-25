aa228c4b29dbfc1bce4833d71e6dc236f96543a9
merret <buurman@dkrz.de>
2019-01-25 20:39:32 +0100

    Make sure sys stats are written in one line.

--- info_collectors_cronjob/system_stats.sh
+++ info_collectors_cronjob/system_stats.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
 STAT_WORK_DIR="/var/lib/irods/msiExecCmd_bin"
-/var/lib/irods/msiExecCmd_bin/system_analyser.sh | jq '[to_entries | .[]] | {sysinfo: .}'  1>>${STAT_WORK_DIR}/system_stats.json  2>>${STAT_WORK_DIR}/system_stats-error.log
+/var/lib/irods/msiExecCmd_bin/system_analyser.sh | jq '[to_entries | .[]] | {sysinfo: .}'  1>${STAT_WORK_DIR}/system_stats.json.tmp  2>>${STAT_WORK_DIR}/system_stats-error.log
+echo $(cat ${STAT_WORK_DIR}/system_stats.json.tmp) >> ${STAT_WORK_DIR}/system_stats.json
 
+# The last line writes everything into one single line, which is better for reading the log by Filebeat
+# TODO: Remove the tmp file.
