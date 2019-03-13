b40b1022a502ad310a3e563cc8064bd6e7397187
merret <buurman@dkrz.de>
2019-03-13 17:06:49 +0100

    Cosmetics: Comments.

--- info_collectors_cronjob/quota_stats.sh
+++ info_collectors_cronjob/quota_stats.sh
@@ -3,9 +3,12 @@
 STAT_WORK_DIR="/var/lib/irods/msiExecCmd_bin"
 
 # Collect stats and write to log
+# Only the last event is kept in the "quota.json"
+# All events are kept in the "quota-error.log" (TODO: Rotation)
 ${STAT_WORK_DIR}/quota_stats_collector.py 1>${STAT_WORK_DIR}/quota.json 2>${STAT_WORK_DIR}/quota-error.log
 
 # Also send to RabbitMQ -or- write to Filebeat-supervised log
 message=`cat ${STAT_WORK_DIR}/quota.json`
 ${STAT_WORK_DIR}/rabbitclient.py seadatacloud accounting_stats ${message}
 
+# Note that error messages are not sent anywhere!
--- info_collectors_cronjob/system_stats.sh
+++ info_collectors_cronjob/system_stats.sh
@@ -6,6 +6,8 @@ STAT_WORK_DIR="/var/lib/irods/msiExecCmd_bin"
 /var/lib/irods/msiExecCmd_bin/system_analyser.sh | jq '[to_entries | .[]] | {sysinfo: .}'  1>${STAT_WORK_DIR}/system_stats.json.tmp  2>>${STAT_WORK_DIR}/system_stats-error.log
 
 # Write everything into one single line, which is better for reading the log by Filebeat
+# Only the last event is kept in the "system_stats.json"
+# All events are kept in the "system_stats-error.log" (TODO: Rotation)
 echo $(cat ${STAT_WORK_DIR}/system_stats.json.tmp) > ${STAT_WORK_DIR}/system_stats.json
 rm ${STAT_WORK_DIR}/system_stats.json.tmp
 
@@ -13,3 +15,4 @@ rm ${STAT_WORK_DIR}/system_stats.json.tmp
 message=`cat ${STAT_WORK_DIR}/system_stats.json`
 ${STAT_WORK_DIR}/rabbitclient.py seadatacloud system_stats ${message}
 
+# Note that error messages are not sent anywhere!
