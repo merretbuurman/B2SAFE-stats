df528f83ac78e5665bd49a7d87ba2e5c78a8b86f
merret <buurman@dkrz.de>
2019-03-13 16:59:23 +0100

    Cosmetics: Comments, etc.

--- info_collectors_cronjob/quota_stats.sh
+++ info_collectors_cronjob/quota_stats.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
 STAT_WORK_DIR="/var/lib/irods/msiExecCmd_bin"
+
+# Collect stats and write to log
 ${STAT_WORK_DIR}/quota_stats_collector.py 1>${STAT_WORK_DIR}/quota.json 2>${STAT_WORK_DIR}/quota-error.log
 
--- info_collectors_cronjob/system_stats.sh
+++ info_collectors_cronjob/system_stats.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
 STAT_WORK_DIR="/var/lib/irods/msiExecCmd_bin"
-/var/lib/irods/msiExecCmd_bin/system_analyser.sh | jq '[to_entries | .[]] | {sysinfo: .}'  1>${STAT_WORK_DIR}/system_stats.json.tmp  2>>${STAT_WORK_DIR}/system_stats-error.log
-echo $(cat ${STAT_WORK_DIR}/system_stats.json.tmp) >> ${STAT_WORK_DIR}/system_stats.json
-# The last line writes everything into one single line, which is better for reading the log by Filebeat
 
+# Collect stats and write to log
+/var/lib/irods/msiExecCmd_bin/system_analyser.sh | jq '[to_entries | .[]] | {sysinfo: .}'  1>${STAT_WORK_DIR}/system_stats.json.tmp  2>>${STAT_WORK_DIR}/system_stats-error.log
+
+# Write everything into one single line, which is better for reading the log by Filebeat
+echo $(cat ${STAT_WORK_DIR}/system_stats.json.tmp) >> ${STAT_WORK_DIR}/system_stats.json
 rm ${STAT_WORK_DIR}/system_stats.json.tmp
