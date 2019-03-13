5ad4d2ca694e18b883c0fbeb5b2be84b49b32376
merret <buurman@dkrz.de>
2019-03-13 17:02:42 +0100

    Add rabbitclient to cronjob scripts, so RabbitMQ is possible.

--- info_collectors_cronjob/quota_stats.sh
+++ info_collectors_cronjob/quota_stats.sh
@@ -5,3 +5,7 @@ STAT_WORK_DIR="/var/lib/irods/msiExecCmd_bin"
 # Collect stats and write to log
 ${STAT_WORK_DIR}/quota_stats_collector.py 1>${STAT_WORK_DIR}/quota.json 2>${STAT_WORK_DIR}/quota-error.log
 
+# Also send to RabbitMQ -or- write to Filebeat-supervised log
+message=`cat ${STAT_WORK_DIR}/quota.json`
+${STAT_WORK_DIR}/rabbitclient.py seadatacloud accounting_stats ${message}
+
--- info_collectors_cronjob/system_stats.sh
+++ info_collectors_cronjob/system_stats.sh
@@ -6,5 +6,10 @@ STAT_WORK_DIR="/var/lib/irods/msiExecCmd_bin"
 /var/lib/irods/msiExecCmd_bin/system_analyser.sh | jq '[to_entries | .[]] | {sysinfo: .}'  1>${STAT_WORK_DIR}/system_stats.json.tmp  2>>${STAT_WORK_DIR}/system_stats-error.log
 
 # Write everything into one single line, which is better for reading the log by Filebeat
-echo $(cat ${STAT_WORK_DIR}/system_stats.json.tmp) >> ${STAT_WORK_DIR}/system_stats.json
+echo $(cat ${STAT_WORK_DIR}/system_stats.json.tmp) > ${STAT_WORK_DIR}/system_stats.json
 rm ${STAT_WORK_DIR}/system_stats.json.tmp
+
+# Also send to RabbitMQ -or- write to Filebeat-supervised log
+message=`cat ${STAT_WORK_DIR}/system_stats.json`
+${STAT_WORK_DIR}/rabbitclient.py seadatacloud system_stats ${message}
+
